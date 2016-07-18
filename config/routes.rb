92d58d7c8b603243757b61dc10aea90c96cd7d51
Ji You <kroyagis@gmail.com>
2016-07-18 16:02:19 -0400

    routes completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:new, :create]
+
+  resources :texts do
+    resources :comments, only: [:show, :create, :destroy]
+  end
+
+  resources :videos do
+    resources :comments, only: [:show, :create, :destroy]
+  end
+
+  resources :photos do
+    resources :comments, only: [:show, :create, :destroy]
+  end
+
 end
