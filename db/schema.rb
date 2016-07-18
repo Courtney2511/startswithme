f23c80713a7e9a16c60c87473bd3e4486d1dd17a
Keira Rochester <cheesypoophs@gmail.com>
2016-07-18 16:48:21 -0400

    Added password and password confirmation columns to the user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160718185824) do
+ActiveRecord::Schema.define(version: 20160718204343) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "comment"
@@ -41,8 +41,10 @@ ActiveRecord::Schema.define(version: 20160718185824) do
     t.string   "name"
     t.string   "email"
     t.string   "profile_picture"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
+    t.string   "password"
+    t.string   "password_confirmation"
   end
 
   create_table "videos", force: :cascade do |t|
