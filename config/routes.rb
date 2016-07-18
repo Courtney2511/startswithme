8a2573a8fc86e17ce1c1a2c76903b5d6895d601e
Keira Rochester <cheesypoophs@gmail.com>
2016-07-18 16:38:54 -0400

    completed users controller and added root 'users#index'. Created sign up form, and show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'users#index'
+
+  resources :users, only: [:new, :create]
+
+  resources :texts do
+    resources :comments, only: [:show, :create, :destroy]
+  end
+
+  resources :videos do
+    resources :comments, only: [:show, :create, :destroy]
+  end
+
+  resources :photos do
+    resources :comments, only: [:show, :create, :destroy]
+  end
+
 end
