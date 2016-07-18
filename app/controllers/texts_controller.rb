8b5ac3a2a575d4c42ace1fcff8f53de1d7d74cc7
Ji You <kroyagis@gmail.com>
2016-07-18 15:55:02 -0400

    text controller finished

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,2 +1,29 @@
 class TextsController < ApplicationController
+  def index
+    @texts = Text.all
+  end
+
+  def new
+    @text = Text.new
+  end
+
+  def create
+    @text = Text.new(text_params)
+    if @text.save
+      redirect_to texts_url
+    else
+      render :new
+    end
+  end
+
+  def show
+    @text = Text.find(params[:id])
+  end
+
+  private
+
+  def text_params
+    params.require(:text).permit(:text)
+  end
+  
 end
