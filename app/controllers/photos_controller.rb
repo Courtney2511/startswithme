cd84571ad9d9eb709f4a3a0d334da08512ccf292
Ji You <kroyagis@gmail.com>
2016-07-18 16:13:58 -0400

    completed photo controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,28 @@
 class PhotosController < ApplicationController
+  def index
+    @photos = Photo.all
+  end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+    if @photo.save
+      redirect_to photos_url
+    else
+      render :new
+    end
+  end
+
+  def show
+    @photo = Photo.find(params[:id])
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:photo, :caption)
+  end
 end
