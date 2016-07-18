b6681286700eddcdc3f434e773e0c60863372441
Ji You <kroyagis@gmail.com>
2016-07-18 16:12:10 -0400

    merged master and finished video controller

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,2 +1,28 @@
 class VideosController < ApplicationController
+  def index
+    @videos = Video.all
+  end
+
+  def new
+    @video = Video.new
+  end
+
+  def create
+    @video = Video.new(video_params)
+    if @video.save
+      redirect_to videos_url
+    else
+      render :new
+    end
+  end
+
+  def show
+    @video = Video.find(params[:id])
+  end
+
+  private
+
+  def video_params
+    params.require(:video).permit(:video, :caption)
+  end
 end
