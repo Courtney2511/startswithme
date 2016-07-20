58617d0afcc4d2373eb0cb60513f68dedb4b57f1
Ji You <kroyagis@gmail.com>
2016-07-20 16:38:32 -0400

    changed body column from string to text

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160720185729) do
+ActiveRecord::Schema.define(version: 20160720201924) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "comment"
@@ -33,9 +33,9 @@ ActiveRecord::Schema.define(version: 20160720185729) do
   create_table "posts", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "category"
-    t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "body"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
