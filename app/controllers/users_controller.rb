ecac74f13a62c0b61d7ed35c7800c36cb7a5835a
Ji You <kroyagis@gmail.com>
2016-07-21 15:46:39 -0400

    click on user name to go to profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to(:users, notice: 'Profile created!') #root_path is protected to logged in users only.  
+      redirect_to(:users, notice: 'Profile created!') #root_path is protected to logged in users only.
       # send the user a welcome email
       UserMailer.welcome_email(@user).deliver_later
     else
