6426b7906063fef225dda4e3b8a21a53806d4bb1
Courtney Noonan <courtneynoonan@me.com>
2016-07-21 14:09:56 -0400

    adding simple password authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path, notice: "Signed in!"
+      redirect_to root_path
       # send the user a welcome email
       UserMailer.welcome_email(@user).deliver_later
     else
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :crypted_password, :password, :password_confirmation)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160721171047) do
+ActiveRecord::Schema.define(version: 20160721174804) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "comment"
@@ -43,10 +43,12 @@ ActiveRecord::Schema.define(version: 20160721171047) do
     t.string   "name"
     t.string   "email"
     t.string   "profile_picture"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
     t.string   "salt"
     t.string   "crypted_password"
+    t.string   "password"
+    t.string   "password_confirmation"
     t.index ["email"], name: "index_users_on_email"
   end
 
