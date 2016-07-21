542c73fcb46c997eeaa154321ee8429f5c743893
Courtney Noonan <courtneynoonan@me.com>
2016-07-21 13:13:13 -0400

    adapting users table to sorcery requirements

--- db/migrate/20160721135555_sorcery_core.rb
+++ db/migrate/20160721135555_sorcery_core.rb
@@ -1,13 +1,9 @@
 class SorceryCore < ActiveRecord::Migration
   def change
-    create_table :users do |t|
-      t.string :email,            :null => false
-      t.string :crypted_password
-      t.string :salt
-
-      t.timestamps
-    end
-
-    add_index :users, :email, unique: true
+    add_column :users, :salt, :string
+    add_column :users, :crypted_password, :string
   end
+
+    add_index :users, :email # unique: true
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160720201924) do
+ActiveRecord::Schema.define(version: 20160721171047) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "comment"
@@ -43,11 +43,11 @@ ActiveRecord::Schema.define(version: 20160720201924) do
     t.string   "name"
     t.string   "email"
     t.string   "profile_picture"
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
-    t.string   "password"
-    t.string   "password_confirmation"
-    t.string   "password_digest"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.string   "salt"
+    t.string   "crypted_password"
+    t.index ["email"], name: "index_users_on_email"
   end
 
 end
