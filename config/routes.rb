8c53d84b6d638fb46851cd2fa533e635e6b570db
Ji You <kroyagis@gmail.com>
2016-07-22 12:29:51 -0400

    -changed landing page session/new -if user is logged in, redirect to posts -worked on user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'posts#index'
+  root 'sessions#new'
 
-  resources :users
-  # , only: [:new, :create]
+  resources :users, only: [:new, :show, :create]
 
   resources :posts do
     resources :photos, only: [:show, :create, :destroy]
