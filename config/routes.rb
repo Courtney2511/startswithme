f4316af73a0599740916187635f29296efb07338
Courtney Noonan <courtneynoonan@me.com>
2016-07-25 13:33:42 -0400

    completing password reset feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'password_resets/create'
-
-  get 'password_resets/edit'
-
-  get 'password_resets/update'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'posts#index'
+
+  resources :password_resets
 
   resources :users
   # , only: [:new, :create]
