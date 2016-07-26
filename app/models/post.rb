e39aba851c0953b15537cb457cc588de5dcfc225
Ji You <kroyagis@gmail.com>
2016-07-26 15:47:47 -0400

    validates only the title

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,13 +7,6 @@ class Post < ApplicationRecord
   #mounting the iamge uploader to be used with the post model
   mount_uploader :image, ImageUploader
 
-  validate :title_or_link_or_image
+  validates :title, presence: true
 
-  private
-
-  def title_or_link_or_image
-    if [self.title, self.link, self.image].reject(&:blank?).size == 0
-      errors[:base] << ("Please enter at least one field")
-    end
-  end
 end
