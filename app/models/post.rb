e13f8cad2940dbfc48ec8041578eb530e6815132
Ji You <kroyagis@gmail.com>
2016-07-26 15:18:58 -0400

    added validation method to accept title or link or image

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,14 @@ class Post < ApplicationRecord
 
   #mounting the iamge uploader to be used with the post model
   mount_uploader :image, ImageUploader
+
+  validates :title_or_link_or_image
+
+  private
+
+  def title_or_link_or_image
+    if [self.title, self.link, self.image].reject(&:blank?).size == 0
+      errors[:base] << ("Please enter at least one field")
+    end
+  end
 end
