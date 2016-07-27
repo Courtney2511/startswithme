c77f9e8b5c9429f2dacfbc11277ecd5d67f1463d
Stella <stellaha4@gmail.com>
2016-07-27 16:32:24 -0400

    moved scripts from new.html.erb on users and sesions page to a js file

---
+++ app/assets/javascripts/sessions.js
@@ -0,0 +1,50 @@
+
+$( document ).ready(function() {
+// LOGIN PAGE
+  $('.signin').on('click', function(event){
+    event.preventDefault();
+    // modal will display on click
+    $('.modal')
+      .css("display","block");
+    });
+    // modal will close when 'x' is clicked
+  $('.close').on('click', function(){
+    $('.modal')
+      .css("display","none");
+    });
+  // prevent default if user doesn't input anything into fields
+  $('.actions').on('click', function(){
+    // if there's no value in <input>, then prevent default on login
+    if($("#email").val() == "" || $("#password").val() == ""){
+      event.preventDefault();
+      $('.session-input').addClass("error");
+    }
+    $('.session-input').on('click',function(){
+    $(this).removeClass('error');
+    });
+  });
+
+  // SIGN UP
+    $('.signup').on('click', function(event){
+      event.preventDefault();
+      // modal will display on click
+      $('.modal')
+        .css("display","block");
+      });
+      // modal will close when 'x' is clicked
+    $('.close').on('click', function(){
+      $('.modal')
+        .css("display","none");
+      });
+    // prevent default if user doesn't input anything into fields
+    $('.actions').on('click', function(){
+      // if there's no value in <input>, then prevent default on login
+      if($("#user_email").val() == "" || $("#user_password").val() == "") || $("#user_password_confirmation").val() == "") || $("#user_name").val() == ""){
+        event.preventDefault();
+        $('.session-input').addClass("error");
+      }
+      $('.session-input').on('click',function(){
+      $(this).removeClass('error');
+      });
+    });
+});
