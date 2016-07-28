ae721ffc4143c94f8136cad6416366062203b350
Stella <stellaha4@gmail.com>
2016-07-27 23:38:42 -0400

    created a border around post content and changed the font sizes and colors

--- app/assets/javascripts/sessions.js
+++ app/assets/javascripts/sessions.js
@@ -25,17 +25,17 @@ $( document ).ready(function() {
   });
 
   // SIGN UP
-    $('.signup').on('click', function(event){
-      event.preventDefault();
-      // modal will display on click
-      $('.modal-signup')
-        .css("display","block");
-    });
-      // modal will close when 'x' is clicked
-    $('.close').on('click', function(){
-      $('.modal-signup')
-        .css("display","none");
-    });
+    // $('.signup').on('click', function(event){
+    //   event.preventDefault();
+    //   // modal will display on click
+    //   $('.modal-signup')
+    //     .css("display","block");
+    // });
+    //   // modal will close when 'x' is clicked
+    // $('.close').on('click', function(){
+    //   $('.modal-signup')
+    //     .css("display","none");
+    // });
     //prevent default if user doesn't input anything into fields
     // $('.actions').on('click', function(){
     //   // if there's no value in <input>, then prevent default on login
