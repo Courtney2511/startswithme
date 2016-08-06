7b0cb5c5b4f4b84fe436d07821380802dfa08e26
Stella <stellaha4@gmail.com>
2016-08-06 12:37:21 -0400

    completed shrinking effect on banner

--- app/assets/javascripts/sessions.js
+++ app/assets/javascripts/sessions.js
@@ -1,23 +1,6 @@
 $( document ).ready(function() {
 // LOGIN PAGE
-// creates a pulse effect when either input fields are clicked
-//  $('input#email').on('click',function(){
-//     $('input#email').addClass('animated pulse');
-//     if ($('input#email').hasClass('animated pulse')){
-//       $('input#password').removeClass('animated pulse');
-//     }
-//   })
-//
-// $('input#password').on('click',function(){
-//   $('input#password').addClass('animated pulse');
-//   if ($('input#password').hasClass('animated pulse')){
-//     $('input#email').removeClass('animated pulse');
-//   }
-// })
-
-  // if form is filled out correctly, the
   $('.actions').on('click',function(){
     $('.actions').addClass('animated pulse');
   });
-
 });
