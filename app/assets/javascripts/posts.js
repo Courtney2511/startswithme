7fe6416ff37b57b5e8903ff2778d43ad38bc75a4
Stella <stellaha4@gmail.com>
2016-08-06 13:49:58 -0400

    removed the banner image resize - it was really annoying me

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,11 +1,11 @@
-
-$( document ).ready(function() {
-    $(window).scroll(function() {
-        var scroll = $(window).scrollTop();
-        if (scroll >= 500) {
-            $(".new-post-banner").addClass('smaller');
-        } else {
-            $(".new-post-banner").removeClass('smaller');
-        }
-    });
-});
+// 
+// $( document ).ready(function() {
+//     $(window).scroll(function() {
+//         var scroll = $(window).scrollTop();
+//         if (scroll >= 500) {
+//             $(".new-post-banner").addClass('smaller');
+//         } else {
+//             $(".new-post-banner").removeClass('smaller');
+//         }
+//     });
+// });
