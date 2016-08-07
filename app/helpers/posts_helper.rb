56de9f4e7532f87b352e36a06fd380a7b16e9246
Ji You <kroyagis@gmail.com>
2016-08-06 23:16:26 -0400

    -fixed the empty video issue

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,8 +1,10 @@
 module PostsHelper
-  #Module for embeding a youtube link that a user inputs 
+  #Module for embeding a youtube link that a user inputs
   def embed(link)
-    youtube_id = link.split("=").last
-    content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{youtube_id}")
+
+      youtube_id = link.split("=").last
+      content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{youtube_id}")
+
   end
 
 end
