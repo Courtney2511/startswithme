b4d97a84e8e67100625f3ec930f630d04f24d715
Keira Rochester <cheesypoophs@gmail.com>
2016-08-08 15:40:23 -0400

    changed created_at format for posts and comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,33 @@ class Comment < ApplicationRecord
 
   #mounting the iamge uploader to be used with the comment model
   mount_uploader :profile_picture, ProfilePictureUploader
+
+  def hours
+    ((Time.now - created_at)/1.hour).round
+  end
+
+  def convert_hours
+    if hours == 0
+      'less than 1'
+    elsif hours < 1
+      (hours * 60).round
+    elsif hours > 24
+      (hours / 24).round
+    else
+      hours
+    end
+  end
+
+  def min_hour_day
+    if hours == 0
+      return 'minute'
+    elsif hours < 1
+      return 'minutes'
+    elsif hours > 24
+      return 'day'
+    else
+      return 'hour'
+    end
+  end
+
 end
