51a7037a71a5d6f698942cd80f925129350284ee
Ji You <kroyagis@gmail.com>
2016-08-08 15:13:02 -0400

    -prevents empty comment from being posted

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,6 @@ class Comment < ApplicationRecord
 
   #mounting the iamge uploader to be used with the comment model
   mount_uploader :profile_picture, ProfilePictureUploader
+
+  validates :comment, presence: true
 end
