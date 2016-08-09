9d2b6a52989f87734577b50a93a7b487c517ba1b
Ji You <kroyagis@gmail.com>
2016-08-09 10:45:58 -0400

    -Edit comment is now working

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,7 @@ class Comment < ApplicationRecord
   belongs_to :user
 
   #mounting the iamge uploader to be used with the comment model
-  mount_uploader :profile_picture, ProfilePictureUploader
+  # mount_uploader :profile_picture, ProfilePictureUploader
 
   validates :comment, presence: true
 end
