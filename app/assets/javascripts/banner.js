69c228206da73fc0d6482141b11d68e586b30b14
Stella <stellaha4@gmail.com>
2016-08-11 15:10:20 -0400

    went into all pages to change banner

---
+++ app/assets/javascripts/banner.js
@@ -0,0 +1,24 @@
+// $(document).ready(function(){
+//   /* when on mobile view, show a different banner */
+//   var $window = $(window);
+//
+//   function changeWindowSize (){
+//     var windowSize = $window.width();
+//     if (windowSize < 520){
+//       console.log("less than 520")
+//       /*old banner will disappear*/
+//       $('#new-post-banner-section').css('display', 'none');
+//       /*new banner will appear*/
+//       $('.mobile-banner-container').css('display','block');
+//     } else if (windowSize > 521){
+//       console.log("bigger than 520")
+//       $('#new-post-banner-section').css('display',"");
+//       $('.mobile-banner-container').css('display','none');
+//     }
+//   }
+//
+//   changeWindowSize();
+//
+//   $(window).resize(changeWindowSize);
+//
+// });
